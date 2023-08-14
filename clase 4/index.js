9ea9c61f30f74b9818e1104d019a502e79aa13a6
Exequiel <exepulgar@gmail.com>
2023-08-14 00:20:35 -0400

    PreEntrega n1

---
+++ clase 4/index.js
@@ -0,0 +1,26 @@
+const estudiante = {
+  nombre: 'Tomas',
+  apellido: 'Zalazar',
+  edad: 25,
+}
+
+const jugador1 = {
+  nombre: 'Maradona',
+  velocidadEnKph: 30,
+  posicion: 'delantero',
+  equipo: 'sacachispas'
+}
+
+const jugador2 = {
+  nombre: 'Julio',
+  velocidadEnKph: 29,
+  posicion: 'delantero',
+  equipo: 'sacachispas'
+}
+
+const jugador3 = {
+  nombre: 'Dibu',
+  velocidadEnKph: 28,
+  posicion: 'defensa',
+  equipo: 'sacachispas'
+}
